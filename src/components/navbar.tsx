019256d0bfba1f68ac5ceb678068d8f115b2287f
Archie Habaradas <habaradas.archie@cogitate.io>
2023-07-05 08:00:34 +0800

    fix: navbar not displaying brand logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ export const NavBar = () => (
         <Image 
           width={32}
           height={32}
-          src={"/icons/mg-brand-logo.png"}
+          src={"/icons/brand-logo.png"}
           alt="brand"
         />
       </div>
