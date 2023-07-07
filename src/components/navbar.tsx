1b7e09bdfaaf63f88cdb5332e25cbe17cb71f5e7
ryan <gimotea.ryan@cogitate.io>
2023-07-07 08:42:37 +0800

    fixed: home link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ export const NavBar = () => (
   <div className="py-4 relative sticky top-0 backdrop-blur-sm bg-white/30 z-50"> 
     <div className="max-w-xl-2 m-auto flex justify-between 
                   z-50 px-10 max-w-screen-ab-xl mx-auto  text-space-blue-100">
-      <div className="mr-2 flex self-center">
+      <a href="/" className="mr-2 flex self-center">
         <Image 
           width={32}
           height={32}
@@ -14,7 +14,7 @@ export const NavBar = () => (
           className="mr-2 "
         />
         <h6 className="font-squada">MATH GRELLIC</h6>
-      </div>
+      </a>
       <div className="space-x-12 btn-text-md-bold">
         <a href="/about-us">About Math Grellic</a>
         <a href="">Training</a>
